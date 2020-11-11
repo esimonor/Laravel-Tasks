3a75f459da522803152e5f4bc6483f973f187477
esimonor <esimonor18dw@ikzubirimanteo.com>
2020-11-11 14:23:32 +0100

    Adds tasks to DB

--- tasks/routes/web.php
+++ tasks/routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::resource('/', TaskController::class);
+
+Route::get('/', 'TaskController@index');
+Route::post('/create', 'TaskController@store');
+Route::get('/delete/{id}', 'TaskController@destroy');
